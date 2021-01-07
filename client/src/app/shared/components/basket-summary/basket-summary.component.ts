de0ec9bd1f3353d2152770af97f53d9004980545
nmarino74 <io_2004@tiscali.it>
2021-01-07 12:56:38 +0100

    End of section 19

---
+++ client/src/app/shared/components/basket-summary/basket-summary.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { IBasket, IBasketItem } from '../../models/basket';
+import { BasketService } from '../../../basket/basket.service';
+
+@Component({
+  selector: 'app-basket-summary',
+  templateUrl: './basket-summary.component.html',
+  styleUrls: ['./basket-summary.component.scss']
+})
+export class BasketSummaryComponent implements OnInit {
+  basket$: Observable<IBasket>;
+  @Output() decrement: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
+  @Output() increment: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
+  @Output() remove: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
+  @Input() isBasket = true;
+
+  constructor(private basketService: BasketService) { }
+
+  ngOnInit() {
+    this.basket$ = this.basketService.basket$;
+  }
+
+  decrementItemQuantity(item: IBasketItem) {
+    this.decrement.emit(item);
+  }
+
+  incrementItemQuantity(item: IBasketItem) {
+    this.increment.emit(item);
+  }
+
+  removeBasketItem(item: IBasketItem) {
+    this.remove.emit(item);
+  }
+
+}
